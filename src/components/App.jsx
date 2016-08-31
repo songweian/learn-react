1d1f4328536f9f3cc533393c7821c8fdd36c6181
weian404 <weian404@gmail.com>
2016-08-31 15:36:53 +0800

    init with antd-init

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+import Todos from './Todos/Todos';
+import MainLayout from '../layouts/MainLayout/MainLayout';
+
+const App = ({ location }) => {
+  return (
+    <MainLayout>
+      
+      <div>hello world</div>
+    </MainLayout>
+  );
+};
+
+App.propTypes = {
+};
+
+export default App;
